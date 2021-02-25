db64c9afa2f36201b42940db8a8b887a24170eaa
unknown <patryk.maternicki@gmail.com>
2021-02-25 09:17:45 +0100

    add unit test for request validator

--- helpers/requestValidator.js
+++ helpers/requestValidator.js
@@ -5,8 +5,13 @@ const methods = {
 module.exports = {
   valid: (query, validMethod)  => {
     let isValid = true
+
+    if(Object.keys(query).length === 0 && Object.keys(validMethod).length > 0) {
+      return false
+    }
+
     for(let prop of Object.keys(query)) {
-      var fields = validMethod[prop]
+      var fields = validMethod[prop] || []
       var value = query[prop]
       for(let i = 0; i < fields.length; i++) {
         if(!methods[fields[i]](value)) {
